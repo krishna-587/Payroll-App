7b56afce05d6b3e7d4b0e8f7c6d438243f8fa36c
Krishna gopal singh <krishnagopalsingh587@gmail.com>
2025-03-05 22:27:57 +0530

    [Krishna Gopal Singh] Add. Added Payroll-App commited

---
+++ Payroll-App/src/test/java/com/example/Payroll/App/PayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Payroll.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
